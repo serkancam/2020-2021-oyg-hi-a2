cad656cda9bbf9834dfd2f7e09797004ce5d8daa
serkancam <sekocam@gmail.com>
2021-03-11 23:58:59 +0300

    der10032021

--- opencv/kontur_bulma.py
+++ opencv/kontur_bulma.py
@@ -1,9 +1,11 @@
 import os
 import cv2
 import numpy as np
-yol = os.path.join(os.getcwd(),"opencv","images","chp2","elmalar.jpg")
+yol = os.path.join(os.getcwd(),"opencv","images","chp2","elmalar3.jpg")
 resim = cv2.imread(yol)
-gri=cv2.cvtColor(resim,cv2.COLOR_BGR2GRAY)
+# resim2 = cv2.bilateralFilter(resim,11,50,50)
+gri=cv2.cvtColor(resim
+,cv2.COLOR_BGR2GRAY)
 _,sbResim = cv2.threshold(gri,110,255,cv2.THRESH_BINARY)
 konturlar,_=cv2.findContours(sbResim,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 ## elle onturları çizdirmek
@@ -19,6 +21,6 @@ konturlar,_=cv2.findContours(sbResim,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 print(len(konturlar))
 for knt in konturlar:
     deger = cv2.approxPolyDP(knt,0.009*cv2.arcLength(knt,True),True)
-    cv2.drawContours(resim,[deger],0,(0,255,0),5)
+    cv2.drawContours(resim,[deger],0,(0,255,0),2)
 cv2.imshow("resim",resim)
 cv2.waitKey(0)
